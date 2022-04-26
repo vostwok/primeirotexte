5ca3ba5a328fa022e3e0b5e5574255f4dc4c450c
vostwok <vladimivostwok007@gmail.com>
2022-04-26 18:20:02 -0300

    atualizando codigo

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const botaoMobile = document.getElementById('botao-mobile');
 
+const botaoCampany = document.getElementById('botton-campany');
+
+const botaoFeatures = document.getElementById('botton-features');
+
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
@@ -13,5 +17,27 @@ function toggleMenu(event) {
   }
 }
 
+function toggleMenuFeatures(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const menu1 = document.getElementById('menu1');
+  menu1.classList.toggle('active')
+  const active = menu1.classList.contains('active');
+  event.currenTarget.setAttribute('aria-expanded', active);
+}
+
+function toggleMenuCampany(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const menu2 = document.getElementById('menu2');
+  menu2.classList.toggle('active')
+  const active = menu2.classList.contains('active');
+  event.currenTarget.setAttribute('aria-expanded', active);
+}
+
 botaoMobile.addEventListener('click', toggleMenu);
-botaoMobile.addEventListener('touchstart', toggleMenu)
+botaoMobile.addEventListener('touchstart', toggleMenu);
+
+botaoFeatures.addEventListener('click', toggleMenuFeatures);
+botaoFeatures.addEventListener('touchstart', toggleMenuFeatures);
+
+botaoCampany.addEventListener('click', toggleMenuCampany);
+botaoCampany.addEventListener('touchstart', toggleMenuCampany);
